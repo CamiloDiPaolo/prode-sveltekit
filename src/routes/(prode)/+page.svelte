a3fe2fef0c357a23c278d1607fab072bd6e3d3dc
Camilo Di Paolo <camilodipaolo10@gmail.com>
2022-12-01 22:39:45 -0300

    claramente no es el last commit, pero era graciosos ponerlo

--- src/routes/(prode)/+page.svelte
+++ src/routes/(prode)/+page.svelte
@@ -1,8 +1,3 @@
-<script>
-	export let data;
-</script>
-
-<!-- {data.equipos} -->
 <div class="h-full">
 	<p class="fixed z-40 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
 		El prode anterior se rompio 🥳
